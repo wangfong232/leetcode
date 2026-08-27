e02b7d81f0c2fa8244533e2ece1eb793e8834af9
wangfong232 <tranquangphong232@gmail.com>
2026-08-27 23:42:42 +0700

    feat: implement Binary Tree Preorder Traversal solution

---
+++ Java/tree/144-binary-tree-preorder-traversal.java
@@ -0,0 +1,112 @@
+/*
+ * @lc app=leetcode id=144 lang=java
+ *
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.67%)
+ * Likes:    9011
+ * Dislikes: 237
+ * Total Accepted:    2.6M
+ * Total Submissions: 3.4M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the preorder traversal of its nodes'
+ * values.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,2,3]
+ * 
+ * Output: [1,2,3]
+ * 
+ * Explanation:
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+ * 
+ * Output: [1,2,4,5,6,7,3,8,9]
+ * 
+ * Explanation:
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * 
+ * Output: []
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: root = [1]
+ * 
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * Time complexity: O(n) - Every node is visited exactly once.
+     * Space complexity: O(h) - Where h is the height of the tree (O(logn) for balanced trees)
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+
+    private void traverse(TreeNode root, List<Integer> result){
+        if(root==null) return;
+        result.add(root.val);
+        traverse(root.left, result);
+        traverse(root.right, result);
+    }
+}
+// @lc code=end
+
